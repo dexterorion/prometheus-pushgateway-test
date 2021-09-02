62ec43b73becf84ec94f3273c21da2c44510dd18
Vinicius Nordi <viniciusnordiesperanca@gmail.com>
2021-09-02 10:44:04 -0300

    intercepting api request

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/dexterorion/prometheus-pushgateway-test
 go 1.16
 
 require (
+	github.com/emicklei/go-restful/v3 v3.5.2 // indirect
 	github.com/joho/godotenv v1.3.0 // indirect
 	github.com/prometheus/client_golang v1.11.0 // indirect
 	github.com/prometheus/pushgateway v1.4.1 // indirect
--- main.go
+++ main.go
@@ -3,46 +3,38 @@ package main
 import (
 	"fmt"
 	"log"
-	"math/rand"
+	"net/http"
 	"os"
 	"time"
 
+	restful "github.com/emicklei/go-restful/v3"
 	"github.com/joho/godotenv"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/push"
 )
 
+type User struct {
+	Id   int
+	Name string
+}
+
 var (
-	completionTime = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "db_backup_last_completion_timestamp_seconds",
-		Help: "The timestamp of the last completion of a DB backup, successful or not.",
-	})
-	successTime = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "db_backup_last_success_timestamp_seconds",
-		Help: "The timestamp of the last successful completion of a DB backup.",
-	})
 	duration = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "db_backup_duration_seconds",
-		Help: "The duration of the last DB backup in seconds.",
-	})
-	records = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "db_backup_records_processed",
-		Help: "The number of records processed in the last DB backup.",
+		Name: "request_duration_seconds",
+		Help: "The duration of the request.",
 	})
 )
 
-func performBackup() (int, error) {
-	// Perform the backup and return the number of backed up records and any
-	// applicable error.
-	// ...
-
-	wait := rand.Intn(1000)
-	time.Sleep(time.Duration(wait) * time.Millisecond)
-
-	return 42, nil
-}
+//
+// This example shows how to log both the request body and response body
 
 func main() {
+	restful.Add(newUserService())
+	log.Print("start listening on localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func newUserService() *restful.WebService {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
@@ -57,30 +49,56 @@ func main() {
 	// We use a registry here to benefit from the consistency checks that
 	// happen during registration.
 	registry := prometheus.NewRegistry()
-	registry.MustRegister(completionTime, duration, records)
+	registry.MustRegister(duration)
 	// Note that successTime is not registered.
 
-	pusher := push.New(promgate, "db_backup").Gatherer(registry)
+	pusher := push.New(promgate, "request_type").Gatherer(registry)
 
-	start := time.Now()
-	n, err := performBackup()
-	records.Set(float64(n))
-	// Note that time.Since only uses a monotonic clock in Go1.9+.
-	duration.Set(time.Since(start).Seconds())
-	completionTime.SetToCurrentTime()
-	if err != nil {
-		fmt.Println("DB backup failed:", err)
-	} else {
-		// Add successTime to pusher only in case of success.
-		// We could as well register it with the registry.
-		// This example, however, demonstrates that you can
-		// mix Gatherers and Collectors when handling a Pusher.
-		pusher.Collector(successTime)
-		successTime.SetToCurrentTime()
-	}
-	// Add is used here rather than Push to not delete a previously pushed
-	// success timestamp in case of a failure of this backup.
 	if err := pusher.Add(); err != nil {
+		fmt.Println("Could not add to Pushgateway:", err)
+	}
+
+	if err := pusher.Push(); err != nil {
 		fmt.Println("Could not push to Pushgateway:", err)
+	}
+
+	ws := new(restful.WebService)
+	ws.Filter(HTTPFilter())
+
+	ws.
+		Path("/users").
+		Consumes(restful.MIME_XML, restful.MIME_JSON).
+		Produces(restful.MIME_JSON, restful.MIME_XML)
+
+	ws.Route(ws.POST("/").To(createUser))
+	return ws
+}
+
+// curl -H "content-type:application/json" http://localhost:8080/users -d '{"Id":42, "Name":"Captain Marvel"}'
+//
+func createUser(request *restful.Request, response *restful.Response) {
+	u := new(User)
+	err := request.ReadEntity(u)
+	log.Print("createUser", err, u)
+	response.WriteEntity(u)
+}
+
+func HTTPFilter() restful.FilterFunction {
+	return func(req *restful.Request, resp *restful.Response, chain *restful.FilterChain) {
+		start := time.Now()
+		log.Printf("Starting at... %d\n\n", start.Unix())
+
+		chain.ProcessFilter(req, resp)
+
+		end := time.Now()
+		log.Printf("Finishing at... %d\n\n", end.Unix())
+
+		log.Printf("Diff ... %d\n\n", end.Sub(start).Nanoseconds())
+		log.Printf("Diff float... %f\n\n", float64(end.Sub(start).Nanoseconds()))
+
+		log.Printf("Response is ... %d\n\n", resp.StatusCode())
+
+		// Note that time.Since only uses a monotonic clock in Go1.9+.
+		duration.Set(float64(end.Sub(start).Nanoseconds()))
 	}
 }
